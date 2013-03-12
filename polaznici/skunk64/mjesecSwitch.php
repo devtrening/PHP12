b9aa8d308a78c0bb32cd14b7a6c0caca1073fcb5
Domagoj Tvoric <domagoj.tvoric@gmail.com>
2013-03-12 18:04:58 +0100

    Fileovi sa proslog predavanja.

---
+++ polaznici/skunk64/mjesecSwitch.php
@@ -0,0 +1,61 @@
+<?php
+
+	$mjesec = "8";
+	
+	switch($mjesec) {
+	
+		case 1: 
+			echo "Sijecanj";
+			break;
+			
+		case 2: 
+			echo "Veljaca";
+			break;
+			
+		case 3: 
+			echo "Ozujak";
+			break;
+			
+		case 4: 
+			echo "Travanj";
+			break;
+			
+		case 5: 
+			echo "Svibanj";
+			break;
+			
+		case 6: 
+			echo "Lipanj";
+			break;
+			
+		case 7: 
+			echo "Srpanj";
+			break;
+			
+		case 8: 
+			echo "Kolovoz";
+			break;
+			
+		case 9: 
+			echo "Rujan";
+			break;
+			
+		case 10: 
+			echo "Listopad";
+			break;
+			
+		case 11: 
+			echo "Studeni";
+			break;
+			
+		case 12: 
+			echo "Prosinac";
+			break;
+			
+		default: 
+			echo "Error!";
+			break;
+	
+	}
+
+?>
