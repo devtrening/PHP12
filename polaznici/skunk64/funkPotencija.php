80eadb4f5b02ce82b6a3e25eda83db9969128efc
Domagoj Tvoric <domagoj.tvoric@gmail.com>
2013-03-12 19:38:18 +0100

    Dodan file za izracun potencije pomocu funcije.

---
+++ polaznici/skunk64/funkPotencija.php
@@ -0,0 +1,10 @@
+<?php
+
+	include('funkcija.php');
+	
+	$broj = 8;
+	$potencija = 6;
+	
+	echo potencija($broj, $potencija);
+
+?>
---
+++ polaznici/skunk64/funkcija.php
@@ -0,0 +1,16 @@
+<?php
+
+	function potencija($broj, $potencija) {
+
+		$rezultat = 1;
+
+		for($i=0; $i<$potencija; $i++) {
+			$rezultat *= $broj;
+		}
+		
+		return $rezultat;
+	
+	}
+	
+
+?>
