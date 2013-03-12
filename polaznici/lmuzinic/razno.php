88164e325cc21f669e32fbcff202ddb3868346f0
Luka Muzinic <luka@muzinic.net>
2013-03-12 20:02:58 +0100

    Obrisane potencije.php jer je kod premjesten u funkcije.php Dodan razno.php za demonstraciju funckija.

---
+++ polaznici/lmuzinic/razno.php
@@ -0,0 +1,11 @@
+<?php
+include('info.php');
+include('funkcije.php');
+
+echo "Danas je " . dan(1);
+echo "<br />";
+echo "Mjesec je " . mjesec(3);
+echo "<br />";
+echo "A 2^8 je " . potencija(2, 8);
+
+?>
