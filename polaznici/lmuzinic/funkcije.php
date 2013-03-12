63b3d8a62e42486fac02f910291198455ee7de63
Luka Muzinic <luka@muzinic.net>
2013-03-12 20:02:18 +0100

    Updateane funkcije.php

--- polaznici/lmuzinic/funkcije.php
+++ polaznici/lmuzinic/funkcije.php
@@ -1,19 +1,83 @@
 <?php
-include('info.php');
 
-function potencija($broj, $potencija = 2) {
-
-	// vaš kod
+function potencija($p_broj, $p_potencija = 2) {
 	$rezultat = 1;
-	for ($brojMnozenja = 1; $brojMnozenja <= $potencija; $brojMnozenja ++) {
-		$rezultat *= $broj;
+	for ($brojMnozenja = 1; $brojMnozenja <= $p_potencija; $brojMnozenja ++) {
+		$rezultat *= $p_broj;
 	}
-
 
 	return $rezultat;
 }
 
-function izracunajPorez() {
-
+function dan($p_dan) {
+	switch ($p_dan) {
+		case 0:
+			return "ponedjeljak";
+			break;
+		case 1:
+			return "utorak";
+			break;
+		case 2:
+			return "srijeda";
+			break;
+		case 3:
+			return "cetvrtak";
+			break;
+		case 4:
+			return "petak";
+			break;
+		case 5:
+			return "subota";
+			break;
+		case 6:
+			return "nedjelja";
+			break;
+		default:
+			return false;
+			break;
+	}
 }
-?>
+
+function mjesec($p_mjesec) {
+	switch ($p_mjesec) {
+		case 1:
+			return "sijecanj";
+			break;
+		case 2:
+			return "veljaca";
+			break;
+		case 3:
+			return "ozujak";
+			break;
+		case 4:
+			return "travanj";
+			break;
+		case 5:
+			return "svibanj";
+			break;
+		case 6:
+			return "lipanj";
+			break;
+		case 7:
+			return "srpanj";
+			break;
+		case 8:
+			return "kolovoz";
+			break;
+		case 9:
+			return "rujan";
+			break;
+		case 10:
+			return "listopad";
+			break;
+		case 11:
+			return "studeni";
+			break;
+		case 12:
+			return "prosinac";
+			break;
+		default:
+			return false;
+			break;
+	}
+}
